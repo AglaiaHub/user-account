34ff05719dd22c4867ab2f6bc23d0c8a426a152d
aglaia <aglaia.hub@gmail.com>
2024-02-18 19:32:12 +0200

    Added Principal To Controller

--- src/main/java/java51/useraccount/controller/UserController.java
+++ src/main/java/java51/useraccount/controller/UserController.java
@@ -6,8 +6,10 @@ import java51.useraccount.dto.UpdateUserDto;
 import java51.useraccount.dto.UserDto;
 import java51.useraccount.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.Base64;
 
 @RequiredArgsConstructor
@@ -21,11 +23,16 @@ public class UserController {
         return userService.registerUser(newUserDto);
     }
 
-    @PostMapping("/login")
-    public UserDto login(@RequestHeader("Authorization")  String token){
-        token = token.split(" ")[1];
-        String credentionals = new String(Base64.getDecoder().decode(token));
-        return userService.getUser(credentionals.split(":")[0]);
+//    @PostMapping("/login")
+//    public UserDto login(@RequestHeader("Authorization")  String token){
+//        token = token.split(" ")[1];
+//        String credentionals = new String(Base64.getDecoder().decode(token));
+//        return userService.getUser(credentionals.split(":")[0]);
+//    }
+
+    @PostMapping
+    public UserDto login(Principal principal) {
+        return userService.getUser(principal.getName());
     }
 
     @DeleteMapping("/user/{login}")
@@ -51,5 +58,11 @@ public class UserController {
     @DeleteMapping("/user/{user}/role/{role}")
     public RoleDto deleteRole(@PathVariable("user") String login, @PathVariable String role) {
         return userService.deleteRole(login, role);
+    }
+
+    @PutMapping("/password")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changePassword(Principal principal, @RequestHeader("X-Password") String newPassword){
+        userService.changePassword(principal.getName(), newPassword);
     }
 }
